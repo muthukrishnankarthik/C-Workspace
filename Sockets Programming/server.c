cb217704ff76b737ee01bbb0cc690616cbd450ca
karthik <muthukrishnankarthik@gmail.com>
2020-07-27 10:57:23 +0530

    ADDING NEW FILE

--- Sockets Programming/server.c
+++ Sockets Programming/server.c
@@ -2,26 +2,40 @@
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<string.h>
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
 
 int main()
 {
-	int fd,rb,ra,len;
-	fd= socket(AF_INET,SOCK_STREAM,0);
+	int fd,fd2,rb,ra,len;
+	char  buf[200],html[200]="Message from server";
+fd= socket(AF_INET,SOCK_STREAM,0);
 
 struct sockaddr_in addr;
 struct in_addr adr;
-adr.s_addr=inet_addr("192.168.10.10");
+//adr.s_addr=atons("192.168.10.10");
 
 	addr.sin_family= AF_INET;
-	addr.sin_port= htons(80);
-	addr.sin_addr=adr;
+	addr.sin_port= htons(8080);
+	addr.sin_addr.s_addr=INADDR_ANY;
+
 len=sizeof(addr);
-rb=bind(fd,(struct sockaddr *) &addr,len);
-
+rb=bind(fd,(struct sockaddr *)&addr,len);
+while (1)
+{
 listen(fd,5);
-accept(fd,(struct sockaddr *)&addr,len);
-write(fd,"Welcome to Karthik web!!!",strlen("Welcome to Karthik web!!!"));
 
-while (1);
+if((fd2=accept(fd,(struct sockaddr *)&addr,(socklen_t *)&len)) >0 )
+{
+	printf("Request received from client: ");
+
+ read( fd2 , buf, 1024);
+ printf("%s\n",buf );
+
+send(fd2,html,strlen(html),0);
+}
+
+}
 
 }
